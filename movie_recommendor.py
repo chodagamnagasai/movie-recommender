94eccffbd62ca2c3219f124f793541917e506fe2
Chodagam Surya Bhanu Venkata Naga Sai <chodagamnagasai1@gmail.com>
2026-06-16 11:10:57 +0530

    Change dataset path in movie_recommendor.py
    Updated dataset path for movie recommendations.

--- movie_recommendor.py
+++ movie_recommendor.py
@@ -14,7 +14,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from google.colab import drive
 drive.mount('/content/drive') //add movie dataset path
 import os
-os.chdir('/content/drive/MyDrive/SKILLIT Courses/AI Level 2/Clustering/Movie_recommendation') //add movie dataset path
+os.chdir('dataset path') //add movie dataset path
 df = pd.read_csv("movie_dataset.csv")
 print(df.columns)
 def get_title_from_index(index):
